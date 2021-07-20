d23da520eb01258a3cd89ea3c7f0478c47e9e018
gitzhangzhao <gitzhangzhao@gmail.com>
2021-07-21 00:19:45 +0800

    2021年 07月 21日 星期三 00:19:45 CST

--- test.c
+++ test.c
@@ -1,12 +1,22 @@
 #include "drv.h"
+#include<stdio.h>
+#include<unistd.h>
 
 /* for test */
 int main() {
     int a;
-    read_32(0x6, &a);
-    printf("a:%x\n", a);
-    write_32(0x12345678,0x6);
-    read_32(0x6, &a);
-    printf("a:%x\n", a);
+    /* read_32(0x0300B058, &a); */
+    /* printf("gpio PC12:%lx\n", a); */
+    while (1) {
+        write_32(0x00001000, 0x0300B058);
+        read_32(0x0300B058, &a);
+        printf("gpio PC12:0x%08x\n", a);
+        sleep(1);
+
+        write_32(0x00002000, 0x0300B058);
+        read_32(0x0300B058, &a);
+        printf("gpio PC12:0x%08x\n", a);
+        sleep(1);
+    }
     return 0;
 }
--- test.py
+++
@@ -1,8 +0,0 @@
-from pygments.styles import *
-for style in get_all_styles():
-    command = 'dashboard -style syntax_highlighting {!r}'.format(style)
-    gdb.execute(command)
-    print(command)
-    if input('Use this style? (y/N) ') == 'y':
-        break
-end
